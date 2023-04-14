6dde3728e84e845a1c764f3b98e01a5e0c0c1238
coutinho21 <guilhermecscoutinho@gmail.com>
2023-04-14 18:15:07 +0100

    Added Home Page

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once('templates/common.php');
-
-    outputHeader();
-
-    outputFooter();
-?>
--- templates/common.php
+++ templates/common.php
@@ -9,12 +9,20 @@
             <title>Trouble Tickets</title>
             <meta charset="UTF-8">
             <meta name="css/viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="css/styles.css">
         </head>
 
         <body>
             <header>
-                <h1>Trouble Tickets</h1>
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Trouble <br /> Tickets</a></li>
+                        <li><img class="profile" src="icons/user.png"/></li>
+                    </ul>
+                </nav>
             </header>
+            <main>
+
 <?php 
     } 
 
