2f6a2ead3d7ebace148c1366508e0038e1e0412e
coutinho21 <guilhermecscoutinho@gmail.com>
2023-04-19 10:13:15 +0100

    Update on database

--- database/trouble_tickets.sql
+++ database/trouble_tickets.sql
@@ -12,7 +12,6 @@ CREATE TABLE users (
     name VARCHAR,                                                  -- name of the user
     email VARCHAR UNIQUE,                                          -- email address
     password VARCHAR,                                              -- password hash
-    department VARCHAR,                                            -- department of the user
     role VARCHAR CHECK (role IN ('client', 'agent', 'admin'))      -- role of the user
 );
 
@@ -23,32 +22,43 @@ CREATE TABLE departments (
 
 -- many-to-many relationship between users and departments
 CREATE TABLE users_departments (
-    user VARCHAR REFERENCES user(username),                        -- username of the user
-    department_id INTEGER REFERENCES department(id)                -- department id
+    user VARCHAR REFERENCES users(username),                       -- username of the user
+    department_id INTEGER REFERENCES departments(id)               -- department id
 );
 
 CREATE TABLE tickets (
   id INTEGER PRIMARY KEY,                                          -- ticket id
-  client INTEGER REFERENCES user(username),                        -- client id
-  agent INTEGER REFERENCES user(username),                         -- agent id 
-  status VARCHAR CHECK(status IN ('open', 'assigned', 'closed')),  -- status of the ticket
+  client INTEGER REFERENCES users(username),                       -- client id
+  agent INTEGER REFERENCES users(username),                        -- agent id 
+  status VARCHAR,                                                  -- status of the ticket
   department VARCHAR,                                              -- ticket department
   title VARCHAR,                                                   -- title of the article
   date INTEGER,                                                    -- date when the article was published in epoch format
   introduction VARCHAR,                                            -- an introductory paragraph
-  fullVARCHAR VARCHAR                                              -- the rest of the VARCHAR
+  description VARCHAR                                              -- the rest of the VARCHAR
 );
 
-CREATE TABLE ticket_hashtags (
+CREATE TABLE hashtags (
   id INTEGER PRIMARY KEY,                                          -- ticket_hashtag id
-  ticket_id INTEGER REFERENCES ticket(id),                         -- ticket id
-  hashtag_name VARCHAR                                             -- hashtag name
+  name VARCHAR                                                     -- hashtag name
 );
 
 CREATE TABLE ticket_history (
   id INTEGER PRIMARY KEY,                                          -- ticket_history id
-  ticket_id INTEGER REFERENCES ticket(id),                         -- ticket id
-  user INTEGER REFERENCES user(username),                          -- user id
+  ticket_id INTEGER REFERENCES tickets(id),                        -- ticket id
+  user INTEGER REFERENCES users(username),                         -- user id
   action VARCHAR,                                                  -- action
   action_date VARCHAR                                              -- action date
+);
+
+-- many-to-many relationship between tickets and hashtags
+CREATE TABLE tickets_hashtags (
+  ticket_id INTEGER REFERENCES tickets(id),                        -- ticket id
+  hashtag_id INTEGER REFERENCES hashtags(id)                       -- hashtag id
+);
+
+CREATE TABLE faqs (
+  id INTEGER PRIMARY KEY,                                          -- faq id
+  question VARCHAR,                                                -- frequently asked question
+  answer VARCHAR                                                   -- the answer to that question
 );
