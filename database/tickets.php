48f7b5ec76738f616ca337f5cf7209507fc81fad
coutinho21 <guilhermecscoutinho@gmail.com>
2023-05-03 22:56:49 +0100

    Add new database query

--- database/tickets.php
+++ database/tickets.php
@@ -6,4 +6,11 @@
         $stmt->execute();
         return $stmt->fetchAll();
     }
+
+    function getTicket($id) {
+        $db = getDatabaseConnection();
+        $stmt = $db->prepare('SELECT * FROM tickets WHERE id = ?');
+        $stmt->execute(array($id));
+        return $stmt->fetch();
+    }
 ?>
