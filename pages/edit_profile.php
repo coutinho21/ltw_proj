390965e544b5828d6c4620660ab4a6cef0f281e1
coutinho21 <guilhermecscoutinho@gmail.com>
2023-05-07 02:33:20 +0100

    Implemented edit profile view

---
+++ pages/edit_profile.php
@@ -0,0 +1,15 @@
+<?php
+    require_once(__DIR__ . '/../templates/common.php');
+    require_once(__DIR__ . '/../templates/profile.php');
+    require_once(__DIR__ . '/../database/users.php');
+
+    session_start();
+
+    if(!isset($_SESSION['username']))
+        header('Location: index.php');
+
+    $user = getUser($_SESSION['username']);
+    outputHeader(); 
+    outputEditProfile($user);
+    outputFooter();
+?>
--- templates/profile.php
+++ templates/profile.php
@@ -4,7 +4,7 @@
         <main class="profile-info">
             <div class="header-edit-profile">
                 <h1>Profile</h1>
-                <button>Edit profile</button>
+                <button onclick="window.location.href='edit_profile.php'">Edit profile</button>
             </div>
             <div>
                 <label>name</label>
@@ -24,6 +24,29 @@
             </div>
             <a href="../actions/action_logout.php" style="text-align: center; color: #000;">Logout</a>
         </main>
+<?php
+    }
+
+    function outputEditProfile($user){
+?>
+    <main class="edit-profile">
+        <h1>Edit profile</h1>
+        <form class="edit-profile-form" method="post" action="../actions/action_edit_profile.php">
+            <label>name<br/>
+                <input value="<?=$user['name']?>"></input> <br/><br/>
+            </label>
+            <label>username<br/>
+                <input value="<?=$user['username']?>"></input> <br/><br/>
+            </label>
+            <label>email<br/>
+                <input value="<?=$user['email']?>"></input> <br/><br/>
+            </label> 
+            <div class="cancel-done">
+                <button onclick="window.location.href='profile.php'">Cancel</button>
+                <button type="submit">Done</button>
+            </div>
+        </form>
+    </main>
 <?php
     }
 ?>
